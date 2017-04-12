14d693eb7c76c6889c4d8f58b757c2bc597906b7
steveoh <sgourley@utah.gov>
2017-04-11 19:03:48 -0600

    Release v1.4.0

--- _src/app/config.js
+++ _src/app/config.js
@@ -44,7 +44,7 @@ define([
     let recreation = 'https://services1.arcgis.com/99lidPhWCzftIe9K/arcgis/rest/services/Recreation/';
 
     var config = {
-        version: '1.3.2',
+        version: '1.4.0',
         urls: {
             bikeonstreet: {
                 url: recreation + 'FeatureServer/9',
